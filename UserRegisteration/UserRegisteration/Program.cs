ce74a7ce7ae8ff4291562e2213111f66e0a5907f
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-01-06 14:34:07 +0530

    [Prashant]UserRegistration : Valid first name

---
+++ UserRegisteration/UserRegisteration/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UserRegisteration;
+
+namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration Validating using Regular Expression!");
+            RegexSample regexSample = new RegexSample();
+            regexSample.Validating();
+        }
+    }
+}
---
+++ UserRegisteration/UserRegisteration/RegexSample.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegisteration
+{
+    public class RegexSample
+    {
+        string pattern = "^[A-Za-z]{2,}$";
+        public void Validating()
+        {
+            Regex regex = new Regex(pattern);
+            Console.WriteLine("Validating the first name");
+            ValidatingFirstName();
+
+        }
+        public void ValidatingFirstName()
+        {
+            Regex Regex = new Regex(pattern);
+            Console.WriteLine("Enter the first name:");
+            string input = Console.ReadLine();
+            bool res = Regex.IsMatch(input);
+            if (res)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
